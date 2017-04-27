f161c0ef633a9cc95af95af7804b78d4b17d1295
iuri aranda <iuri.aranda@gmail.com>
2017-04-28 00:14:53 +0200

    Removed stacks, it's migrated to skyscrapers/skyscrapers-terraform

--- stacks/ecs-cluster/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ecs_cluster" {
-  value = "${data.terraform_remote_state.static.ecs_cluster_name}"
-}
--- stacks/ecs-cluster/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "project" {
-  default = "concourse"
-}
-
-variable "aws_region" {
-  default = "eu-west-1"
-}
