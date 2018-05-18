e5e5ac70d921f9279c31f180e2300f9a450ebeb4
iuri aranda <iuri.aranda@gmail.com>
2018-05-18 09:20:29 +0200

    Always create the cross-account role in keys module

--- keys/cross-account.tf
+++ keys/cross-account.tf
@@ -27,14 +27,12 @@ data "aws_iam_policy_document" "concourse_keys_cross_account_policy" {
 }
 
 resource "aws_iam_role" "concourse_keys_cross_account" {
-  count              = "${length(var.concourse_keys_cross_account_principals) > 0 ? 1 : 0}"
   name_prefix        = "concourse-keys-"
   description        = "This role is meant to be assumed by Concourse workers on other AWS accounts to be able to access the Concourse keys. Setup: ${var.name}"
   assume_role_policy = "${data.aws_iam_policy_document.concourse_keys_cross_account_assume_role_policy.json}"
 }
 
 resource "aws_iam_role_policy" "concourse_keys_cross_account" {
-  count  = "${length(var.concourse_keys_cross_account_principals) > 0 ? 1 : 0}"
   role   = "${aws_iam_role.concourse_keys_cross_account.name}"
   policy = "${data.aws_iam_policy_document.concourse_keys_cross_account_policy.json}"
 }
