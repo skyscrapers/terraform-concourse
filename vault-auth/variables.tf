eb439a410162f750e3437bc2cb4b71d03a0bb922
iuri aranda <iuri.aranda@gmail.com>
2018-10-05 14:54:41 +0200

    Configure the concourse vault role to issue periodic tokens (#37)
    * Configure the concourse vault role to issue periodic tokens
    
    Currently, concourse can't use the AWS auth method to retrieve a Vault 
    token, so we retrieve the token before starting the concourse web 
    process via the Vault cli, and then provide that token to the concourse 
    process. Because of that Concourse can't get a new token when this one 
    expires, and the whole process has to be restarted. To avoid this, we've 
    configured the Vault auth role to issue periodic tokens, which never 
    expire as long as they are renewed in time (which concourse already 
    does).
    
    As per https://github.com/skyscrapers/engineering/issues/104
    
    * Fix description

--- vault-auth/variables.tf
+++ vault-auth/variables.tf
@@ -3,19 +3,24 @@ variable "vault_concourse_role_name" {
 }
 
 variable "additional_vault_policies" {
-  description = "Additional Vault policies to attach to the Concourse role. Defaults to empty list"
+  description = "Additional Vault policies to attach to the Concourse role"
   default     = []
 }
 
 variable "concourse_iam_role_arn" {
-  description = "IAM role ARN of the Concourse ATC server"
+  description = "IAM role ARN of the Concourse ATC server. You can get this from the concourse web module outputs"
 }
 
 variable "vault_aws_auth_backend_path" {
-  description = "The path the AWS auth backend being configured was mounted at. Defaults to aws."
+  description = "The path the AWS auth backend being configured was mounted at"
   default     = "aws"
 }
 
 variable "vault_server_url" {
   description = "The Vault server url"
+}
+
+variable "vault_token_period" {
+  description = "Vault token renewal period, in seconds. This sets the token to never expire, but it still has to be renewed within the duration specified by this value"
+  default     = 43200
 }
