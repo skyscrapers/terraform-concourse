42a89914f30f40ab5e4c6e54942516c1ff3ec122
iuri aranda <iuri.aranda@gmail.com>
2018-12-04 18:12:06 +0100

    Add some tests to the concourse module and other test structural changes
    As per https://github.com/skyscrapers/engineering/issues/149
    
    This commit adds some "useful" tests
    and sligthly changes the test structure
    (terraform and fly binaries are provided by a concourse resource)

---
+++ examples/basic/outputs.tf
@@ -0,0 +1,11 @@
+output "concourse_hostname" {
+  value = "${module.concourse_web.concourse_hostname}"
+}
+
+output "concourse_local_user_username" {
+  value = "${var.concourse_auth_username}"
+}
+
+output "concourse_local_user_password" {
+  value = "${var.concourse_auth_password}"
+}
--- test/basic_example_test.go
+++ test/basic_example_test.go
@@ -9,7 +9,25 @@ import (
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/test-structure"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/gruntwork-io/terratest/modules/logger"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/skyscrapers/terraform-concourse/test/fly"
 )
+
+var (
+	flyBinaryPath = getenv("TEST_FLY_PATH", "fly")
+	flyTargetName = "test"
+)
+
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
 
 // An example of how to test the simple Terraform module in examples/basic using Terratest.
 func TestBasicExample(t *testing.T) {
@@ -48,7 +66,44 @@ func TestBasicExample(t *testing.T) {
 		terraform.InitAndApply(t, terraformOptions)
 	})
 
-	// test_structure.RunTestStage(t, "validate", func() {
-		// initializeAndUnsealVaultCluster(t)
-	// })
+	test_structure.RunTestStage(t, "validate", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, exampleFolder)
+		validate(t, terraformOptions)
+	})
+}
+
+func validate(t *testing.T, terraformOptions *terraform.Options) {
+	var flyCommand fly.Command
+
+	concourse_hostname := terraform.Output(t, terraformOptions, "concourse_hostname")
+	concourse_user := terraform.Output(t, terraformOptions, "concourse_local_user_username")
+	concourse_pass := terraform.Output(t, terraformOptions, "concourse_local_user_password")
+
+	flyCommand = fly.NewCommand(flyTargetName, t, flyBinaryPath)
+
+	loginInConcourse(t, flyCommand, concourse_hostname, "main", concourse_user, concourse_pass, true)
+	validateWorkers(t, flyCommand, 1)
+	setPipeline(t, flyCommand, "test", "./test_pipeline.yaml", make([]string, 0))
+}
+
+func loginInConcourse(t *testing.T, flyCommand fly.Command, concourse_hostname string, concourse_team string, concourse_user string, concourse_pass string, insecure bool) {
+	logger.Logf(t, "Logging into Concourse server %s, team %s, username %s", concourse_hostname, concourse_team, concourse_user)
+	flyCommand.Login(fmt.Sprintf("https://%s", concourse_hostname), concourse_team, concourse_user, concourse_pass, insecure)
+}
+
+func validateWorkers(t *testing.T, flyCommand fly.Command, workersCount int) {
+	logger.Logf(t, "Check that there are %d running workers attached to Concourse", workersCount)
+	workers := flyCommand.Workers()
+
+	for _, w := range workers {
+		assert.Equal(t, "running", w.State, fmt.Sprintf("worker %s should be running", w.Name))
+	}
+
+	assert.Equal(t, workersCount, len(workers), fmt.Sprintf("there should be %d running workers", workersCount))
+}
+
+func setPipeline(t *testing.T, flyCommand fly.Command, pipeline_name string, configFilePath string, varsFilePaths []string) {
+	logger.Logf(t, "Setting pipeline %s in Concourse", pipeline_name)
+	flyCommand.SetPipeline(pipeline_name, configFilePath, varsFilePaths)
+	flyCommand.UnpausePipeline(pipeline_name)
 }
