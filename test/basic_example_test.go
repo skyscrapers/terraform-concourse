441d0923837d4f792e950881cedba04b99776c5c
iuri aranda <iuri.aranda@gmail.com>
2018-12-05 15:16:09 +0100

    Add extra logging in tests

--- test/basic_example_test.go
+++ test/basic_example_test.go
@@ -107,6 +107,8 @@ func validateWorkers(t *testing.T, flyCommand fly.Command, workersCount int) {
 			return "", errors.New(fmt.Sprintf("the number of running workers (%s) doesn't match the expected value (%s)", len(workers), workersCount))
 		}
 
+		logger.Logf(t, "correct number of running workers found: %s", len(workers))
+
 		for _, w := range workers {
 			if w.State != "running" {
 				return "", errors.New(fmt.Sprintf("worker %s is not in the 'running' state (%s)", w.Name, w.State))
