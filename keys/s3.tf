d9b2b0da95cf6a99ff525753df9f05a2ae84c407
iuri aranda <iuri.aranda@gmail.com>
2019-02-19 12:08:28 +0100

    Apply server-side encryption by default in keys bucket

--- keys/s3.tf
+++ keys/s3.tf
@@ -7,6 +7,14 @@ resource "aws_s3_bucket" "concourse_keys" {
     enabled = true
   }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags {
     Name        = "concourse keys for ${var.name}"
     Environment = "${var.environment}"
