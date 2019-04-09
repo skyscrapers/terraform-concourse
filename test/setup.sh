3f1fbdc060a70aaaef6f0ead1b1393f40413353f
iuri aranda <iuri.aranda@gmail.com>
2019-04-09 18:43:42 +0200

    Adapt worker boot script to new install method (#53)
    * Adapt worker boot script to new install method
    
    As per https://github.com/skyscrapers/engineering/issues/204
    
    * Adapt tests and concourse web for version 5
    
    With this, tests will download and use fly cli from the tested concourse.
    
    Also adapted the tests according to the current go standards.
    
    * Add concourse-release input back
    
    Needed to fetch the concourse version
    
    * Support previous versions of concourse

--- test/setup.sh
+++ test/setup.sh
@@ -5,8 +5,3 @@ apk add --update unzip curl dep git
 # Unzip terraform binary
 unzip terraform-release/terraform_*_linux_amd64.zip -d /usr/local/bin
 chmod +x /usr/local/bin/terraform
-
-# Move fly binary into PATH
-export TEST_FLY_PATH=/usr/local/bin/fly
-cp -a concourse-release/fly_linux_amd64 $TEST_FLY_PATH
-chmod +x $TEST_FLY_PATH
