9072b5a0a65901723fd2a18e6accccd04833252d
Amany El-Guindy <minniux@gmail.com>
2019-08-07 16:51:15 +0200

    update vpc module

--- examples/basic/basic_network.tf
+++ examples/basic/basic_network.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source                            = "github.com/skyscrapers/terraform-network//vpc?ref=3.4.1"
+  source                            = "github.com/skyscrapers/terraform-network//vpc?ref=4.0.0"
   cidr_block                        = var.cidr_block
   environment                       = "test"
   project                           = var.project
@@ -7,7 +7,7 @@ module "vpc" {
 }
 
 module "nat_gateway" {
-  source               = "github.com/skyscrapers/terraform-network//nat_gateway?ref=3.4.1"
+  source               = "github.com/skyscrapers/terraform-network//nat_gateway?ref=4.0.0"
   private_route_tables = module.vpc.private_rts
   public_subnets       = module.vpc.public_nat-bastion
 }
