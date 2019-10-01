9567a05b26dda9783b0c042ef3e7b5a9f0e8ef98
Simon Rondelez <simonrondelez@me.com>
2019-10-01 09:12:24 +0200

    Bump concourse version to 5.5.3 per engineering#300

--- ec2-worker/variables.tf
+++ ec2-worker/variables.tf
@@ -107,7 +107,7 @@ variable "concourse_worker_instance_count" {
 
 variable "concourse_version" {
   description = "Concourse CI version to use. Defaults to the latest tested version"
-  default     = "5.5.1"
+  default     = "5.5.3"
   type        = string
 }
 
