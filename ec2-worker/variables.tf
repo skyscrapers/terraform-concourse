7ef5dd2b9cee80cd532d54e18760443dc9fdcfcf
Simon Rondelez <simonrondelez@me.com>
2020-03-24 17:38:19 +0100

    Patch for CVE-2018-15798

--- ec2-worker/variables.tf
+++ ec2-worker/variables.tf
@@ -107,7 +107,7 @@ variable "concourse_worker_instance_count" {
 
 variable "concourse_version" {
   description = "Concourse CI version to use. Defaults to the latest tested version"
-  default     = "5.8.0"
+  default     = "5.8.1"
   type        = string
 }
 
