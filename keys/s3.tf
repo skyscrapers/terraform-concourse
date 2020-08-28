6031b9be045576d030fa9b9af4e63dd7f010d677
Philip Dubois <hello@philipdubois.be>
2020-08-28 11:41:05 +0200

    Explicitly block all public access on keys S3

--- keys/s3.tf
+++ keys/s3.tf
@@ -59,3 +59,10 @@ EOF
 
 }
 
+resource "aws_s3_bucket_public_access_block" "concourse_keys" {
+  bucket                  = aws_s3_bucket.concourse_keys.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
