8cd76bf9bf118e6734124b805fc6b62a804581e3
Philip Dubois <hello@philipdubois.be>
2020-09-08 15:26:36 +0200

    Bump teleport version skyscrapers/engineering#398

--- ec2-worker/variables.tf
+++ ec2-worker/variables.tf
@@ -153,7 +153,7 @@ variable "teleport_auth_token" {
 
 variable "teleport_version" {
   description = "Teleport version for the client"
-  default     = "4.2.10"
+  default     = "4.3.5"
   type        = string
 }
 
