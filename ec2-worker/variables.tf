6dab5f7ed74695b228ba8100b49c465e23a65936
Simon Rondelez <simonrondelez@me.com>
2021-01-18 16:10:06 +0100

    Bump Concourse to 6.7.3 per skyscrapers/egineering#462

--- ec2-worker/variables.tf
+++ ec2-worker/variables.tf
@@ -107,7 +107,7 @@ variable "concourse_worker_instance_count" {
 
 variable "concourse_version" {
   description = "Concourse CI version to use. Defaults to the latest tested version"
-  default     = "6.7.1"
+  default     = "6.7.3"
   type        = string
 }
 
