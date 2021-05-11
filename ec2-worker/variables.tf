ec46f3eee91d21712a22dc6898a478f34fc223a3
Simon Rondelez <simonrondelez@me.com>
2021-05-11 17:20:50 +0200

    Bump Concourse to 7.2.0

--- ec2-worker/variables.tf
+++ ec2-worker/variables.tf
@@ -107,7 +107,7 @@ variable "concourse_worker_instance_count" {
 
 variable "concourse_version" {
   description = "Concourse CI version to use. Defaults to the latest tested version"
-  default     = "7.1.0"
+  default     = "7.2.0"
   type        = string
 }
 
--- ecs-web/variables.tf
+++ ecs-web/variables.tf
@@ -27,7 +27,7 @@ variable "concourse_docker_image" {
 
 variable "concourse_version" {
   description = "Concourse CI version to use. Defaults to the latest tested version"
-  default     = "7.1.0"
+  default     = "7.2.0"
   type        = string
 }
 
