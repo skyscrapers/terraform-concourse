9e8ae431bc23302dd67405ba9309f7f9f37b8cbf
iuri aranda <iuri@skyscrapers.eu>
2021-05-19 16:56:09 +0200

    Adjust Terraform syntax for tf 0.15

--- vault-auth/role.tf
+++ vault-auth/role.tf
@@ -3,6 +3,6 @@ resource "vault_aws_auth_backend_role" "concourse" {
   role                     = var.vault_concourse_role_name
   auth_type                = "iam"
   bound_iam_principal_arns = [var.concourse_iam_role_arn]
-  token_policies           = concat(list(vault_policy.concourse.name), var.additional_vault_policies)
+  token_policies           = concat([vault_policy.concourse.name], var.additional_vault_policies)
   token_period             = var.vault_token_period
 }
