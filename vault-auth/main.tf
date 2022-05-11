2b1b507e241bf6cb31201966e01f8593f9d68eed
iuri aranda <iuri@skyscrapers.eu>
2022-05-11 10:28:18 +0200

    Fix vault-auth module provider dependencies (#68)
    * Fix vault-auth module provider dependencies
    
    It was requiring the aws provider although it wasn't used within the module. Probably a left-over
    
    * Fix provider name

--- vault-auth/main.tf
+++ vault-auth/main.tf
@@ -3,9 +3,9 @@ terraform {
   required_version = "~> 1.0"
 
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 3.74"
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 3.5"
     }
   }
 }
